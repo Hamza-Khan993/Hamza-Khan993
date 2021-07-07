e34b62b746b6fa627a375159410d1a55adfc9dd8
Hamza Khan <hamzakhan99301@gmail.com>
2021-07-07 18:08:21 +0530

    final pushs

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,5 @@ const download = document.getElementById("fileRequest");
 download.addEventListener('click', request);
 
 function request() {
-    window.location = './demoPdf.pdf';
+    window.location = './forms.pdf';
 }
